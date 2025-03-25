7eeeb01c93dd587f6bf00d96209c4762dca2dc20
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-03-25 20:10:37 +0100

    Dodanie metody do Ładowania kontenera

--- Cwiczenie_2/Cwiczenie_2/Container.cs
+++ Cwiczenie_2/Cwiczenie_2/Container.cs
@@ -24,10 +24,28 @@ public class Container
         return $"KON-{type}-{counter:D5}";
     }
     
+    //Ładowanie kontenera
+    public virtual void LoadContainer(double weight)
+    {
+        if (WeightOfCargo + weight > MaxLoad)
+        {
+            throw new OverfillExeption($"Przekroczono maksymalną ładowność konternera {SerialNumber}!");
+        }
+        
+        WeightOfCargo += weight;
+        Console.WriteLine($"Załadowano {weight} kg do kontenera {SerialNumber}");
+    }
+
     //Opróżnianie Contenera
     public virtual void EmptyContainer()
     {
+        if (WeightOfCargo == 0)
+        {
+            Console.WriteLine("Kontener jest pusty");
+        }
+
         WeightOfCargo = 0.0;
+        Console.WriteLine($"Kontener {SerialNumber} został opróżniony");
     }
     
 
