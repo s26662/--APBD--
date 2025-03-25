fc580d5288f483026e876b7e04f2b49330680e91
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-03-25 21:18:24 +0100

    Utworzenie klasy LiquidContainer oraz dodanie metody ładowanie kontenera

---
+++ Cwiczenie_2/Cwiczenie_2/LiquidContainer.cs
@@ -0,0 +1,26 @@
+﻿namespace Cwiczenie_2;
+
+public class LiquidContainer : Container
+{
+    private bool IsHazardous { get; set;}
+
+    public LiquidContainer(bool isHazardous) : base("L")
+    {
+        IsHazardous = isHazardous;
+    }
+
+    public override void LoadContainer(double weight)
+    {
+       double maxAllowedLoad = IsHazardous ? MaxLoad * 0.5 : MaxLoad * 0.9;
+
+       if (weight > maxAllowedLoad)
+       {
+           //Dodanie IHazardNotifier
+       }
+       
+       WeightOfCargo = weight;
+       Console.WriteLine($"Załadowano {weight} kg do kontenera {SerialNumber}");
+    }
+    
+    
+}
