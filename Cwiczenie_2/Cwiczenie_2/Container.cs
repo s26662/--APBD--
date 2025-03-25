24272858b84aee618a23b707e1db3930253d3cd6
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-03-25 19:01:59 +0100

    Inicjalizacja pul oraz utworzenie kontruktora klasy Kontener

--- Cwiczenie_2/Cwiczenie_2/Container.cs
+++ Cwiczenie_2/Cwiczenie_2/Container.cs
@@ -2,6 +2,21 @@
 
 public class Container
 {
+    private static int counter = 0;
+    public string SerialNumber { get; set; } //Numer seryjny
+    public double WeightOfCargo { get; set; } //Masa ładunku
+    public double Height { get; set; } //Wysokość
+    public double Depth { get; set; } //Głębokość
+    public double ContainerWeight { get; set; } //Waga własna
+    public double MaxLoad { get; set; } //Maksymalna ładowność
+
+
+    //Konstruktor 
+    public Container(string type)
+    {
+        SerialNumber = $"KON-{type}-{++counter:D5}";
+    }
+
     
     
 }
