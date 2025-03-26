f83064fc6d8a5d62c4430e2b864a60bdf2d38622
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-03-26 19:02:04 +0100

    Utworzenie klasy Statek

---
+++ Cwiczenie_2/Cwiczenie_2/Ship.cs
@@ -0,0 +1,20 @@
+﻿namespace Cwiczenie_2;
+
+public class Ship
+{
+    private List<Container> _containers = new List<Container>();
+    public string Name { get; set; }
+    public double MaxSpeed { get; set; }
+    public int MaxNumberOfContainers { get; set; }
+    public int MaxWeightOfContainers { get; set; }
+
+    public Ship(string name, double maxSpeed, int maxNumberOfContainers, int maxWeightOfContainers)
+    {
+        Name = name;
+        MaxSpeed = maxSpeed;
+        MaxNumberOfContainers = maxNumberOfContainers;
+        MaxWeightOfContainers = maxWeightOfContainers;
+    }
+    
+
+}
