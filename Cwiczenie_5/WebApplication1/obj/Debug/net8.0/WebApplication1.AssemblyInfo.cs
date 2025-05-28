7820297c6296a15199f0f132e2ae0faca4b6aafc
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-05-28 20:26:26 +0200

    Utworzenie TripController oraz ClientController

--- Cwiczenie_5/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Cwiczenie_5/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac2380a6d46ead87613b2ca21770c540f44f576")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9c67dd16e08f3e07ca4860df43798fd9559240")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
