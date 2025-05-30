c864301ff422f057803c161ba9da29d6af333907
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-05-31 00:19:09 +0200

    Dodanie wymaganych pól w CreateClinetDTO

--- Cwiczenie_5/WebApplication1/Model/DTO/CreateClientDTO.cs
+++ Cwiczenie_5/WebApplication1/Model/DTO/CreateClientDTO.cs
@@ -1,10 +1,17 @@
-﻿namespace WebApplication1.Model.DTO;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Model.DTO;
 
 public class CreateClientDTO
 {
+    [Required]
     public string FirstName { get; set; }
+    [Required]
     public string LastName { get; set; }
+    [Required]
     public string Email { get; set; }
+    [Required]
     public string Telephone { get; set; }
+    [Required]
     public string Pesel { get; set; }
 }
