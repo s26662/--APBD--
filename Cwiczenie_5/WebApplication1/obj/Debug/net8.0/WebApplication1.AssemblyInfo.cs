0ea944c22a446417e74b5a5424691a308a25e4c7
Olaf Chodkiewicz <s26662@pjwstk.edu.pl>
2025-06-13 20:14:54 +0200

    test

--- Cwiczenie_5/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ Cwiczenie_5/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c68d786faab46fef61151b0f3073e53ab1203f59")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09c520cb0f3d173e6850876e9532412353e484d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
